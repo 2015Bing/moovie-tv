583e83f898b5fb1c49ed71cb725c75ad3d9416b5
2015Bing <1126481627@qq.com>
2026-09-22 16:57:31 +0800

    Add Coil image loading for TV posters

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:5.1.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.1.0")
     implementation("com.google.code.gson:gson:2.13.2")
+    implementation("io.coil-kt.coil3:coil-compose:3.6.3")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.6.3")
 }
